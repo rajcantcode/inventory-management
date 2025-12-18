d73e444fc5ec423b8233db96fcb8a64a8c2ecbad
rajcantcode <mahadeshwarraj23@gmail.com>
2025-12-18 18:15:24 +0530

    chore: add port in env file

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
+import dotenv from "dotenv";
 import express from "express";
 import productRoutes from "./routes/products.js";
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 
 app.use("/products", productRoutes);
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
